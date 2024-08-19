1415d5e62243c8519113dee6d627bdd189b7b96f
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2024-08-18 20:09:01 -0400

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cubiTz-cube.github.io',
-  base: 'CubiTzPage',
+  //base: '',
   integrations: []
 });
